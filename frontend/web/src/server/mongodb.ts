6ead2c1a97423ba5cd69c135c848b6f246ca419b
Eduardo Ciciliato <eduardo.ciciliato@gmail.com>
2020-11-30 01:06:13 -0300

    Adds mongodb dbname env

--- frontend/web/src/server/mongodb.ts
+++ frontend/web/src/server/mongodb.ts
@@ -1,15 +1,15 @@
-import url from 'url';
 import { Db, MongoClient } from 'mongodb';
 
 const uri =
-  process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/idlescape-utils?retryWrites=true&w=majority';
+  process.env.MONGO_URL ?? 'mongodb://127.0.0.1:27017/idlescape-utils?retryWrites=true&w=majority';
+const dbname = process.env.MONGO_DBNAME ?? 'idlescape-utils';
 
 let cachedDb: Db;
 
 export const connectToDatabase = async () => {
   if (cachedDb == null) {
     const client = await MongoClient.connect(uri, { useNewUrlParser: true });
-    cachedDb = client.db(url.parse(uri).pathname!.substr(1));
+    cachedDb = client.db(dbname);
   }
   return cachedDb;
 };
